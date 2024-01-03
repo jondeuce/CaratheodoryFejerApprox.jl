c2047efdfa59f46cbf394e29b9e3fb42020cd3b2
Jonathan Doucette <jdoucette@physics.ubc.ca>
2024-01-03 15:49:02 -0800

    run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,25 @@
 using CaratheodoryFejerApprox
 using Documenter
 
-DocMeta.setdocmeta!(CaratheodoryFejerApprox, :DocTestSetup, :(using CaratheodoryFejerApprox); recursive=true)
+DocMeta.setdocmeta!(CaratheodoryFejerApprox, :DocTestSetup, :(using CaratheodoryFejerApprox); recursive = true)
 
 makedocs(;
-    modules=[CaratheodoryFejerApprox],
-    authors="Jonathan Doucette <jdoucette@physics.ubc.ca> and contributors",
-    repo="https://github.com/jondeuce/CaratheodoryFejerApprox.jl/blob/{commit}{path}#{line}",
-    sitename="CaratheodoryFejerApprox.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://jondeuce.github.io/CaratheodoryFejerApprox.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [CaratheodoryFejerApprox],
+    authors = "Jonathan Doucette <jdoucette@physics.ubc.ca> and contributors",
+    repo = "https://github.com/jondeuce/CaratheodoryFejerApprox.jl/blob/{commit}{path}#{line}",
+    sitename = "CaratheodoryFejerApprox.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://jondeuce.github.io/CaratheodoryFejerApprox.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/jondeuce/CaratheodoryFejerApprox.jl",
-    devbranch="main",
+    repo = "github.com/jondeuce/CaratheodoryFejerApprox.jl",
+    devbranch = "main",
 )
